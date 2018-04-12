a38c2c6cebf726e05d81621858d8057dd735fe6f
brianwei1997 <296969208@qq.com>
2018-04-12 13:18:56 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import sys
+import string
+import hashlib
+ 
+if __name__=='__main__':
+  
+  x=1
+  v=raw_input("please enter experiment data:")
+  d=2
+  h=v+str(x)
+  hash=hashlib.sha1()
+  hash.update(bytes(h))
+  value=hash.hexdigest()
+  print value
+  dd=value[0:3]
+  print dd
+  
+  while(dd!='000'):
+   x=x+1
+   h=v+str(x)
+   hash.update(bytes(h))
+   value=hash.hexdigest()
+   print value
+   dd=value[0:3]
+   
+print x 
